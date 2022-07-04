9f4d15a7b0a7680d19b87ba3c551fb382bd3b05e
jubix-pixel <jefenedo4@gmail.com>
2022-07-04 04:16:45 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,5 +18,5 @@ int main(void)
 	printf("size of a long int:%lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(c);
 	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+        return (0);
 }
