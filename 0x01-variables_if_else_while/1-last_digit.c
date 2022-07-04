24af3c422c1dc24fe2eba3410ef84516db02ac7b
jubix-pixel <jefenedo4@gmail.com>
2022-07-04 13:43:31 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 L =  n % 10;
 
 if (L > 5)
-printf("%s %d is %d and is greater than 5\n", str, n. L);
+printf("%s %d is %d and is greater than 5\n", str, n, L);
 else if (L == 0)
 printf("%s %d is %d and is 0\n", str, n, L);
 else if (L > 6)
