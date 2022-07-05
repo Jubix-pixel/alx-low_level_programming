3fd6ca0b234d5c7f105ba36699415dce7f3e0d9a
jubix-pixel <jefenedo4@gmail.com>
2022-07-04 18:16:09 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Prints numbers from 00 to 89
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int d;
+	int i, e;
 
-	for (d = 0; d < 99; d++)
+	i = 48;
+	e = 48;
+
+	while (e < 58)
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 89)
+		i = 48;
+		while (i < 58)
 		{
-			putchar(',');
-			putchar(' ');
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 $$ e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
 		}
+		e++;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
