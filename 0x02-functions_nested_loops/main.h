0f0d3b64e9487d3cf570b1a03056504ea66f307b
jubix-pixel <jefenedo4@gmail.com>
2022-07-05 17:02:11 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,4 @@ int add(int a, int b);
 void print_to_98(int n);                                                        
 void print_times_table(int n);
 void num(int r, int n);
+void print_alphabet(void);
