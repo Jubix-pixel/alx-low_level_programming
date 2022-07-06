f57fe8df338afd4a297217c1e7471ea3682e74c9
jubix-pixel <jefenedo4@gmail.com>
2022-07-05 19:57:08 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,8 +18,7 @@ void print_to_98(int n)
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
-		}
-		else if (n >= 98)
+		} else if (n >= 98)
 		{
 			for (j = n; j >= 98; j--)
 			{
@@ -30,4 +29,4 @@ void print_to_98(int n)
 			}
 		}
 	}
-}
+
