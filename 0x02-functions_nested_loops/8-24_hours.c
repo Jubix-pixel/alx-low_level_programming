65bf18d6e2b032100be9d495ac9f8c32f3feeb7c
jubix-pixel <jefenedo4@gmail.com>
2022-07-05 19:09:04 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,8 +9,8 @@ void jack_bauer(void)
 {
 	int hours = 0;
 	int minutes = 0;
-	int hours_remainders;
-	int min_remainders;
+	int hours_remainder;
+	int min_remainder;
 
 	while (hours <= 23)
 	{
@@ -22,7 +22,7 @@ void jack_bauer(void)
 			_putchar(hours_remainder + '0');
 			_putchar(':');
 			_putchar(minutes / 10 + '0');
-			_putchar(mins_remainder + '0');
+			_putchar(min_remainder + '0');
 			minutes++;
 			_putchar('\n');
 		}
