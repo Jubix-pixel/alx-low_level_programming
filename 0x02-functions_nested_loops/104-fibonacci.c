daa033c78f43f6b7323d8b7cccea78e0bfa64ff1
jubix-pixel <jefenedo4@gmail.com>
2022-07-06 17:22:33 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - finds and prints the first 98 fibonacci numbers
  * starting with 1 and 2
- * followed by a new line 
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
