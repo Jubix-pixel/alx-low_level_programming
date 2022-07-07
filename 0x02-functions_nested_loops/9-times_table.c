b0af70c7283979cab40c638fca2ad17990c4bebe
jubix-pixel <jefenedo4@gmail.com>
2022-07-06 19:23:13 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,11 +17,12 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
-			if (k < 10 && j != 0)
+			if (j != 0 && k < 10)
 			{
 				_putchar('.');
 				_putchar(' ');
 				_putchar(' ');
+			        _putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
