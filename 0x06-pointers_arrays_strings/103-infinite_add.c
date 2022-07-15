73231b7d6d6abeb5bb95288ce72f97fcf6d1bfa0
jubix-pixel <jefenedo4@gmail.com>
2022-07-14 18:35:35 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,11 @@
+#include "maim.h"
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinte_add(char *n1, char 8n2, char *r, int size_r);
+
+/**
+ * add_strings - Adds the numbers stored in two strings.
+ * @n1: The string containing the first number to be added.
+ * *n2: The string containing the second number to be added.
+ *@r: The buffer to store the result.
+ *
