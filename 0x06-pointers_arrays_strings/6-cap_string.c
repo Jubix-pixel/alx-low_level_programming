e469e7637ef1b31e34f8c2d4ee370a24012d25b7
jubix-pixel <jefenedo4@gmail.com>
2022-07-15 16:43:30 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,26 @@
 #include "main.h"
+
 /**
- * cap_string - capitalizes all words of a string.
- * @s: input string.
- * Return: the pointer to dest.
+ * cap_string -> capitalization function.
+ * @x: string param
+ * Return: capitalised version of the string.
  */
-
-char *cap_string(char *s)
+char *cap_string(char *x)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (x[a])
 	{
-		for (i = 0; i < 13; 1++)
+		i = 0;
+		while (i < len)
 		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				x[a] = x[a] - 32;
+			i++;
 		}
-		count++;
+		a++;
 	}
-	return (s);
+	return (x);
 }
