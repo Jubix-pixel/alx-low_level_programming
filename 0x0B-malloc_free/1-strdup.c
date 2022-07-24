7112e2737c0333c9ba5a1599e8ebee51e812f79b
jubix-pixel <jefenedo4@gmail.com>
2022-07-23 18:30:33 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-			for (i = o; str[i] != '\0'; i++)
+			for (i = 0; str[i] != '\0'; i++)
 				;
 
 	strout = (char *)malloc(sizeof(char) * (i + 1));
