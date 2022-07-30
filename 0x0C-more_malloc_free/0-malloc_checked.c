7c620713538a01bea2c224673063c8d22da1b5c9
jubix-pixel <jefenedo4@gmail.com>
2022-07-29 17:54:36 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,7 +7,7 @@
  * Return: Pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
-void malloc_checked(unsigned int b)
+void *malloc_checked(unsigned int b)
 {
 	char *p;
 
