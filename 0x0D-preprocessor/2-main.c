27e4b4c40f16555b097a9727fd6f31bea3ad01e8
jubix-pixel <jefenedo4@gmail.com>
2022-08-01 11:20:11 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Prints the name of the file of the program
- * was compiled from, followed by a new line.
+ *     was compiled from, followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("%s\n", _ _FILE_ _);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
