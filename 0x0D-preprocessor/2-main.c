bde5b50077f86131b6a07ba9091ae633926e5a55
jubix-pixel <jefenedo4@gmail.com>
2022-08-01 12:12:22 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the name of the file of the program
- *     was compiled from, followed by a new line.
+ *        was compiled from, followed by a new line.
  *
  * Return: Always 0.
  */
