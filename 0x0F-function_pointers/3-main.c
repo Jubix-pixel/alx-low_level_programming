1e8218b39d64d32905746637404f6a7e198feeae
jubix-pixel <jefenedo4@gmail.com>
2022-08-03 16:40:23 -0700

    task 2

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,16 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the results of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __atribute__((__unused__)) argc, char *argv[])
+{
+	int num1, num2;
+	char *op;b1
+		b
