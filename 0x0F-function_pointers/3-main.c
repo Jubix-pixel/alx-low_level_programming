b19bad5989d542059f4403ede92fa08032123b58
jubix-pixel <jefenedo4@gmail.com>
2022-08-03 19:34:35 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int __atribute__((__unused__)) argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
