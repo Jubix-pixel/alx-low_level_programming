4ad882162e4cf7cf88410e6c19a59ce68e0f28a5
jubix-pixel <jefenedo4@gmail.com>
2022-08-03 20:00:21 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,11 +3,11 @@
 
 /**
  * get_op_func - Selects the correct function to perform
- * the operation asked by the user.
+ *               the operation asked by the user.
  * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
- * to the operator given as a parameter.
+ *         to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
